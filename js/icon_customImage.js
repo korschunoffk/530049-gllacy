52cd3764b4d4c056bf582397ea6d00f629de1921
korschunoffk <33195448+korschunoffk@users.noreply.github.com>
2017-11-28 16:32:22 +0300

    8.0.5

---
+++ js/icon_customImage.js
@@ -0,0 +1,29 @@
+ymaps.ready(function () {
+    var myMap = new ymaps.Map('map', {
+            center: [59.938587, 30.322954],
+            zoom: 16
+        
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+        MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+            '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+        ),
+
+        myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+            hintContent: 'Собственный значок метки',
+        }, {
+            iconLayout: 'default#image',
+            // Своё изображение иконки метки.
+            iconImageHref: 'img/pin.png',
+            // Размеры метки.
+            iconImageSize: [200, 130],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [-35, -130]
+        });
+   
+    myMap.geoObjects
+        .add(myPlacemark);
+    
+});
