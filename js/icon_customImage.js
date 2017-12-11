21e7c74cc0277f4d9d7e93699216ae20226e76b9
korschunoffk <33195448+korschunoffk@users.noreply.github.com>
2017-12-11 16:50:04 +0300

    8.5
    Правки после проверки

--- js/icon_customImage.js
+++ js/icon_customImage.js
@@ -26,3 +26,38 @@ ymaps.ready(function () {
         .add(myPlacemark);
     myMap.setCenter([59.939001, 30.329037], 16);
 });
+
+        var link = document.querySelector(".contacts_button");
+        var black = document.querySelector(".global-container");
+        
+        var popup = document.querySelector(".feedback");
+        var close = popup.querySelector(".feedback_close");
+        
+        var form = popup.querySelector("feedback_form");
+        var login = popup.querySelector("[name=name]");
+        var email = popup.querySelector("[name=email]");
+        
+        var storage = localStorage.getItem("login");
+        
+        link.addEventListener("click", function (evt) {
+            evt.preventDefault();
+            popup.classList.add("feedback-show");
+            black.classList.add("black");
+            if (storage) {
+                login.value = storage;
+                } else {
+                login.focus();
+                }
+        });
+        
+       close.addEventListener("click", function (evt) {
+            evt.preventDefault();
+            popup.classList.remove("feedback-show");
+            black.classList.remove("black");
+            popup.classList.remove("feedback-error");
+        }); 
+        
+        form.addEventListener("submit", function (evt) {
+            evt.preventDefault();
+            black.classList.remove("black");
+        });
