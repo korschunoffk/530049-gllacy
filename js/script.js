a908791aa30e1e02422b71b4a9d8707be7cec48a
korschunoffk <33195448+korschunoffk@users.noreply.github.com>
2017-12-14 16:02:33 +0300

    9.0 final

--- js/script.js
+++ js/script.js
@@ -29,9 +29,10 @@ var link = document.querySelector(".contacts_button");
 var black = document.querySelector(".global-container");
 var popup = document.querySelector(".feedback");
 var close = popup.querySelector(".feedback_close");
-var form = popup.querySelector("feedback_form");
+var form = popup.querySelector("form");
 var login = popup.querySelector("[name=name]");
 var email = popup.querySelector("[name=email]");
+var text = popup.querySelector("[name=feedback-text]");
 var storage = localStorage.getItem("login");
 link.addEventListener("click", function(evt) {
  evt.preventDefault();
@@ -47,9 +48,15 @@ close.addEventListener("click", function(evt) {
  evt.preventDefault();
  popup.classList.remove("feedback-show");
  black.classList.remove("black");
- popup.classList.remove("feedback-error");
+ popup.classList.remove("modal-error");
 });
-form.addEventListener("submit", function(evt) {
- evt.preventDefault();
- black.classList.remove("black");
+form.addEventListener("submit", function (evt) {
+    if (!login.value || !email.value|| !text.value) {
+      evt.preventDefault();
+      popup.classList.remove("modal-error");
+      popup.offsetWidth = popup.offsetWidth;
+      popup.classList.add("modal-error");
+    } else {
+      black.classList.remove("black");
+    }
 });
